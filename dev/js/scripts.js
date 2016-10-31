2296812a0b03b9dded373d995b2e3a849f68c2ff
Julia Lein <julia.lein@yandex.ru>
2016-10-31 10:33:49 +0300

    доделана секция about (круговая диаграмма) и секция team, изменен bg в хедере

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,31 @@
-$(document).ready(function() {
+var teamItems = document.querySelectorAll('.team__item');
 
-(function(){
+for (var i = 0; i < teamItems.length; i++) {
 
-}());
+  var teamItemImg = teamItems[i].querySelector('.team__item-image');
+  var teamItemContacts = teamItems[i].querySelector('.team__item-contacts');
 
-});
+  teamItemImg.addEventListener('mouseover', function(event){ //при наведении на team__item-image
+    event.preventDefault();
+    this.classList.add('active'); //добавляем класс active team__item-image
+    this.nextElementSibling.classList.add('active'); //добавляем класс active team__item-contacts
+  });
+
+  teamItemImg.addEventListener('mouseout', function(event){
+    event.preventDefault();
+    this.classList.remove('active');
+    this.nextElementSibling.classList.remove('active');
+  });
+
+  teamItemContacts.addEventListener('mouseover', function(event){ //при наведении на team__item-contacts
+    event.preventDefault();
+    this.classList.add('active'); //добавляем класс active team__item-contacts
+    this.previousElementSibling.classList.add('active'); //добавляем класс active team__item-image
+  });
+
+  teamItemContacts.addEventListener('mouseout', function(event){
+    event.preventDefault();
+    this.classList.remove('active');
+    this.previousElementSibling.classList.remove('active');
+  });
+}
